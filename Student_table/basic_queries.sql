111abd905c213b9950c7423eb1d60bb2114e0e5c
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-01-16 17:36:02 +0530

    Create basic_queries.sql
    Added basic queries fies

---
+++ Student_table/basic_queries.sql
@@ -0,0 +1,14 @@
+-- Fetch all record from student table
+SELECT * FROM STUDENT; 
+
+-- Fetch student_name and age
+SELECT STUDENT_NAME, AGE FROM STUDENT; 
+
+-- Fetch id, student_name and age
+SELECT ID, STUDENT_NAME, AGE FROM STUDENT;
+
+-- Fetch id and city
+SELECT ID, CITY FROM STUDENT;
+
+-- Fetch student_name and age
+SELECT STUDENT_NAME, AGE FROM STUDENT;
