3b611544a4421295bb40a6d68ae727204ae98543
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-01-28 17:44:10 +0530

    Create Basics-Query.sql

---
+++ DEPT_EMP-TABLE/Basics-Query.sql
@@ -0,0 +1,29 @@
+--Display all data from EMP
+SELECT * FROM EMP;
+
+--WAQTD name of all the employee
+SELECT ENAME FROM EMP;
+
+--WAQTD name and salary given to all the employee
+SELECT ENAME, SAL FROM EMP;
+
+--WAQTD name and commission given to all the employee
+SELECT ENAME, COMM FROM EMP;
+
+--WAQTD Employee ID and Department number of all the employee
+SELECT EMPNO, DEPTNO FROM EMP;
+
+--WAQTD ENAME and HIREDATE of all the employee
+SELECT ENAME, HIREDATE FROM EMP;
+
+--WAQTD name and job of all the employee
+SELECT ENAME, JOB FROM EMP;
+
+--WAQTD name, job and sal given to all the employee
+SELECT ENAME, JOB, SAL FROM EMP;
+
+--WAQTD DNAMES present in department table
+SELECT DNAME FROM DEPT;
+
+--WAQTD DNAME and LOCATION present in department table
+SELECT DNAME, LOC FROM DEPT;
