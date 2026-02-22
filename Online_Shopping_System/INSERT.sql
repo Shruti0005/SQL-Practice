782d543901f285a44d4b401dc107762c7612fdef
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-02-22 17:34:19 +0530

    Create INSERT.sql

---
+++ Online_Shopping_System/INSERT.sql
@@ -0,0 +1,14 @@
+-- Insert data in the CUSTOMERS table.
+INSERT INTO CUSTOMERS(CUSTOMER_ID, NAME, CITY, EMAIL) 
+  VALUES(1, 'AMIT', 'PUNE', 'AMIT@GMAIL.COM'),
+        (2, 'NEHA', 'MUMBAI', 'NEHA@GMAIL.COM'), 
+        (3, 'RAJ', 'NAGPUR', 'RAJ@GAMIL.COM'), 
+        (4, 'SNEHA', 'PUNE', 'SNEHA@GMAIL.COM'), 
+        (5, 'KARAN', 'DELHI', 'KARAN@GMAIL.COM'),
+        (6, 'NARAYANI', 'NAGPUR', 'NARAYANI@GMAIL.COM'), 
+        (7, 'SHREE', 'HYDERABAD', 'SHREE@GMAIL.COM'), 
+        (8, 'RUHAN', 'BANGLORE', 'RUHAN@GMAIL.COM'), 
+        (9, 'MAHI', 'CHANNAI', 'MAHI@GMAIL.COM'), 
+        (10, 'AVA', 'DELHI', 'AVA@GMAIL.COM');   
+
+-- Insert data in the ORDERS table.
